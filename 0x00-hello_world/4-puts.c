10c8a01bc6de11a92f5d785d8351989fff07ab60
victez_wambui <victezwambui@gmail.com>
2023-03-17 08:30:31 -0700

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+*main - print a line using puts
+*
+*Return: always 0
+*/
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
