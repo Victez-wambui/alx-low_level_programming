d568c7655ab537a2f18163046e736c930f96b3b9
victez_wambui <victezwambui@gmail.com>
2023-03-22 01:38:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - prints the alphabets in lowercase
+*
+*Return: Always 0(success)
+*/
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i <= 122; 1+1)
+	{
+		_putchar(1);
+	}
+	_putchar('\n');
+}
